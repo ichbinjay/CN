7bbc1300eb61975f36447f1a76fc2b48adb71164
ichbinjay <80576787+ichbinjay@users.noreply.github.com>
2022-11-02 17:27:17 +0530

    Create sendImgExp4Server.py

---
+++ sendImgExp4Server.py
@@ -0,0 +1,28 @@
+import socket
+
+ip = "127.0.0.1"
+port = 1230
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((ip, port))
+server.listen(1)
+print("Server started...", end="")
+client, address = server.accept()
+print(f"Connected to {address}")
+
+print("sending file...")
+fp = open("m1.png", "rb")
+
+data = fp.read(1024)
+while data:
+    if client.send(data):
+        data = fp.read(1024)
+        print(fp.tell(), data[:10])
+    else:
+        break
+
+print("done..")
+server.close()
+client.close()
+fp.close()
